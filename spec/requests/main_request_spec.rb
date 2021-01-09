b03a86465969e1a75fe107dfb2e0523d4ff0a410
Nicholas <nnmarques97@gmail.com>
2021-01-08 22:17:04 -0300

    test: main request api version (#1)

--- spec/requests/main_request_spec.rb
+++ spec/requests/main_request_spec.rb
@@ -4,9 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Mains', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
+    before do
       get root_path
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns the api version' do
+      expect(response_json['api_version']).to eq('1.0.0')
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'helpers'
+
 RSpec.configure do |config|
+  # Include helpers module
+  config.include Helpers
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
